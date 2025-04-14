14cdcda601473a31e8fd6f09377280d410edbb31
unknown <200203526+shabil24@users.noreply.github.com>
2025-04-15 00:35:07 +0700

    per 2 route view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile/{nama}/{kelas}/{npm}', 
+[ProfileController::class, 'profile']);
+
+Route::get('/profile', [ProfileController::class, 'profile']);
+
+
+
