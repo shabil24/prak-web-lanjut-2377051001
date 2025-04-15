e8ceed7f14831ef532a9f95ab608eda11672770f
unknown <200203526+shabil24@users.noreply.github.com>
2025-04-15 08:35:59 +0700

    per4 database model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,16 +1,38 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Kelas;
+use App\Models\UserModel;
 
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function create(){
-        return view('create_user');
+
+        return view('create_user', [
+            'kelas' => Kelas::all(),
+        ]);
     }
 
     public function store(Request $request){
+
+        
+$validatedData = $request->validate([
+    'nama' => 'required|string|max:255',
+    'npm' => 'required|string|max:255',
+    'kelas_id' => 'required|exists:kelas,id',
+]);
+
+$user = UserModel::create($validatedData);
+
+$user->load('kelas');
+
+return view('profile', [
+    'nama' => $user->nama,
+    'npm' => $user->npm,
+    'nama_kelas' => $user->kelas->nama_kelas ?? 'Kelas tidak ditemukan',
+]);
 
         $data = [ 
             'nama' => $request->input('nama'), 
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="id">
 <head>
@@ -6,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/asset/style.css">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
+    <title>Profile</title>
 </head>
 <body class="flex items-center justify-center min-h-screen bg-gray-100">
     <div class="bg-white p-6 rounded-lg shadow-lg w-96 text-center">
@@ -17,9 +18,10 @@
             class="w-60 h-60 rounded-full border-4 border-gray-300 object-cover">
         </div>
         <div class="space-y-5 text-left px-6 py-3">
-            <div class="bg-gray-200 py-3 px-4 rounded-md font-semibold">Nama : <?= $nama ?></div>
-            <div class="bg-gray-200 py-3 px-4 rounded-md font-semibold">Kelas : <?= $kelas ?></div>
-            <div class="bg-gray-200 py-3 px-4 rounded-md font-semibold">NPM : <?= $npm ?></div>
+            <div class="bg-gray-200 py-3 px-4 rounded-md font-semibold">Nama  : {{$nama}}</div>
+            <div class="bg-gray-200 py-3 px-4 rounded-md font-semibold">NPM   : {{$npm}}</div>
+            <div class="bg-gray-200 py-3 px-4 rounded-md font-semibold">Kelas : {{$nama_kelas ?? 'kelas tidak ditemukan'}}</div>
+            
         </div>
         
     </div>
